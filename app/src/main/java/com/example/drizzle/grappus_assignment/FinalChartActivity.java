1081435399fa1d0ab317bad8d06cb3bdb7f51428
dhananjay <dhananjayims057@gmail.com>
2018-11-25 22:40:54 +0530

    fixes

--- app/src/main/java/com/example/drizzle/grappus_assignment/FinalChartActivity.java
+++ app/src/main/java/com/example/drizzle/grappus_assignment/FinalChartActivity.java
@@ -1,16 +1,54 @@
 package com.example.drizzle.grappus_assignment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 
 public class FinalChartActivity extends AppCompatActivity {
+    private View viewOne, viewTwo,viewThree, viewFour, viewFive;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_chart);
+        viewOne = findViewById(R.id.view_one);
+        viewTwo = findViewById(R.id.view_two);
+        viewThree = findViewById(R.id.view_three);
+        viewFour = findViewById(R.id.view_four);
+        viewFive = findViewById(R.id.view_five);
+       viewAnimation();
 
+    }
+
+    private void viewAnimation() {
+        final Animation slide_up = AnimationUtils.loadAnimation(this, R.anim.slide_up_animation);
+        final Animation slide_down = AnimationUtils.loadAnimation(this,R.anim.slide_down_animation);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                viewOne.startAnimation(slide_up);
+                viewTwo.startAnimation(slide_up);
+                viewThree.startAnimation(slide_up);
+                viewFour.startAnimation(slide_up);
+                viewFive.startAnimation(slide_up);
+            }
+        }, 500);
+        new Handler().postDelayed(new Runnable(){
+
+            @Override
+            public void run() {
+                viewOne.startAnimation(slide_down);
+                viewTwo.startAnimation(slide_down);
+                viewThree.startAnimation(slide_down);
+                viewFour.startAnimation(slide_down);
+                viewFive.startAnimation(slide_down);
+            }
+        }, 700);
     }
 }
--- app/src/main/java/com/example/drizzle/grappus_assignment/PlanOfActionActivity.java
+++ app/src/main/java/com/example/drizzle/grappus_assignment/PlanOfActionActivity.java
@@ -12,33 +12,95 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.TranslateAnimation;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bluehomestudio.animationplus.animation.HeightAnimation;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.os.Build.TIME;
+
 public class PlanOfActionActivity extends AppCompatActivity{
     LinearLayout wingging_it;
+    TextView tvBringingIt, tvSubText;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_of_action);
         wingging_it = findViewById(R.id.layout_wining);
-        final Animation slide_up = AnimationUtils.loadAnimation(this, R.anim.slide_up_animation);
-        final Animation slide_down = AnimationUtils.loadAnimation(this,R.anim.slide_down_animation);
-        new Handler().postDelayed(new Runnable(){
-
+        tvBringingIt = findViewById(R.id.tv_bringing_it);
+        tvSubText = findViewById(R.id.tv_sub_text);
+//        final Animation slide_up = AnimationUtils.loadAnimation(this, R.anim.slide_up_animation);
+//        final Animation slide_down = AnimationUtils.loadAnimation(this,R.anim.slide_down_animation);
+//        new Handler().postDelayed(new Runnable(){
+//
+//            @Override
+//            public void run() {
+//                wingging_it.startAnimation(slide_up);
+//            }
+//        }, 500);
+//        new Handler().postDelayed(new Runnable(){
+//
+//            @Override
+//            public void run() {
+//                wingging_it.startAnimation(slide_down);
+//            }
+//        }, 700);
+        HeightAnimation heightAnimation = new HeightAnimation(wingging_it , 50 , 450);
+        heightAnimation.setDuration(1000);
+        wingging_it.setAlpha(.2f);
+        wingging_it.startAnimation(heightAnimation);
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                wingging_it.startAnimation(slide_up);
-            }
-        }, 500);
-        new Handler().postDelayed(new Runnable(){
+                HeightAnimation heightAnimation = new HeightAnimation(wingging_it , 450 , 750);
+                heightAnimation.setDuration(1000);
+                Animation animZoomOut = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_out);
+                tvSubText.startAnimation(animZoomOut);
+                wingging_it.setAlpha(.2f);
+                wingging_it.startAnimation(heightAnimation);
 
+
+            }
+        }, 1300);
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                wingging_it.startAnimation(slide_down);
+                HeightAnimation heightAnimation = new HeightAnimation(wingging_it , 750 , 450);
+                heightAnimation.setDuration(1200);
+                Animation animZoomOut = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_out);
+                tvBringingIt.startAnimation(animZoomOut);
+                wingging_it.setAlpha(.2f);
+                wingging_it.startAnimation(heightAnimation);
+
+
             }
-        }, 700);
+        }, 1700);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                HeightAnimation heightAnimation = new HeightAnimation(wingging_it , 450 , 150);
+                heightAnimation.setDuration(1400);
+                wingging_it.setAlpha(.2f);
+                wingging_it.startAnimation(heightAnimation);
+
+
+            }
+        }, 2000);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                HeightAnimation heightAnimation = new HeightAnimation(wingging_it , 150 , 50);
+                heightAnimation.setDuration(2400);
+                wingging_it.setAlpha(.2f);
+                wingging_it.startAnimation(heightAnimation);
+
+
+            }
+        }, 2500);
+
 
         wingging_it.setOnClickListener(new View.OnClickListener() {
             @Override
